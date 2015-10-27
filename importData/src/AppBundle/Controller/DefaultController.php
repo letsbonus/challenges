aef3171bf884a805ba5d0947cee5821cb77fe705
Jaime <jaime@invasionweb.com>
2015-10-27 13:01:27 +0100

    crear formulario

--- importData/src/AppBundle/Controller/DefaultController.php
+++ importData/src/AppBundle/Controller/DefaultController.php
@@ -6,16 +6,39 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use AppBundle\Form\Type\ImportType;
+
 class DefaultController extends Controller
 {
     /**
+     *
+     * Formulario de importación de datos
+     *
+     * @param Request $request
+     * @return Response|\Symfony\Component\HttpFoundation\Response
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
-        
-        return $this->render('AppBundle::defaultView.html.twig', array(
 
+        $form = $this->createForm(new ImportType());
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+
+            // normalizar archivo
+
+
+            // crear datos
+            return $this->render('RitmoAnuncioBundle:Anuncio:fichaGratis.html.twig', array(
+                'anuncio' => $anuncio,
+                'form' => $form->CreateView(),
+                'formFalse' => $formFalse->CreateView(),
+            ));
+
+        }
+
+        return $this->render('AppBundle::defaultView.html.twig', array(
+            'form' => $form->createView()
         ));
     }
 }
