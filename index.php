6d2806bd2f000eaa9b6ba50ea1175cac05216ed6
micamaris <michele.accattoli@amaris.com>
2015-11-21 16:28:49 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="resp.php" method="post" enctype="multipart/form-data">
+    Select file with the data to convert:
+    <input type="file" name="fileToConvert" id="fileToConvert">
+    <input type="submit" value="Convert file" name="submit">
+</form>
+
+</body>
+</html>
