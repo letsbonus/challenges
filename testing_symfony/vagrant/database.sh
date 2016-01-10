197520722f76b08bb5e1a6a7ce185a67d38843dc
JordiCabezudo <jordicabezudo@gmail.com>
2016-01-10 01:44:01 +0100

    fix error on provision to create database

--- testing_symfony/vagrant/database.sh
+++ testing_symfony/vagrant/database.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /var/www_data/testLetsBonus/
+cd /var/www_data/challenge/
 php app/console doctrine:database:create
 php app/console doctrine:generate:entities AppBundle
 php app/console doctrine:schema:update --force
